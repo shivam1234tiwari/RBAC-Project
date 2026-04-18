50103ec10732a0ff06abe2793f87aa70d1195b35
shivam1234tiwari <t.rahul3105@gmail.com>
2026-04-18 21:12:30 +0530

    asign roles

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,31 @@
-import express from 'express';
-import authMiddleware from '../middleware/authMiddleware.js'
-const router=express.Router();
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+import authorizeRoles from "../middleware/roleMiddleware.js";
+const router = express.Router();
 
 // only admin can access this router
 
-router.get('/admin',authMiddleware,(req,res)=>{
-    res.json({message:"Welcome admin"})
-})
+router.get("/admin", authMiddleware, authorizeRoles("admin"), (req, res) => {
+  res.json({ message: "Welcome admin" });
+});
 // both admin and manager can access this router
 
-router.get('/manager',authMiddleware,(req,res)=>{
-    res.json({message:"Welcome manager"})
-})
+router.get(
+  "/manager",
+  authMiddleware,
+  authorizeRoles("admin", "manager"),
+  (req, res) => {
+    res.json({ message: "Welcome manager" });
+  },
+);
 // all can access this router
-router.get('/user',authMiddleware,(req,res)=>{
-    res.json({message:"Welcome user"})
-})
+router.get(
+  "/user",
+  authMiddleware,
+  authorizeRoles("admin", "manager", "user"),
+  (req, res) => {
+    res.json({ message: "Welcome user" });
+  },
+);
 
 export default router;
